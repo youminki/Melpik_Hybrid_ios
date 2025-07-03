e0825270d08a561a4cd1271aabc73f6537b05b74
youminki <dbalsrl7648@gmail.com>
2025-07-03 19:14:29 +0900

    Make setAutoLogin private in LoginManager
    Changed the setAutoLogin method's access level to private to restrict its usage within the LoginManager class.

--- Melpik_ios/Melpik_ios/LoginManager.swift
+++ Melpik_ios/Melpik_ios/LoginManager.swift
@@ -143,7 +143,7 @@ class LoginManager: ObservableObject {
     }
     
     // MARK: - 자동 로그인 설정
-    func setAutoLogin(enabled: Bool) {
+    private func setAutoLogin(enabled: Bool) {
         userDefaults.set(enabled, forKey: "autoLoginEnabled")
     }
     
